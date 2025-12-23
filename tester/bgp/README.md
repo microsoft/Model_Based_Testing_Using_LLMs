3c2c7f8b1789a69de41918763320f0555a1bc658
Rajdeep Mondal <mondalrajdeep814@gmail.com>
2025-12-23 10:23:43 -0800

    updating README

--- tester/bgp/README.md
+++ tester/bgp/README.md
@@ -1,2 +1,30 @@
 ## Building Docker images for BGP implementations
 
+Download all the images from this [link](https://drive.google.com/drive/folders/1fEns5tyxdOZLbICibMnvN71Ucjl2tl89).
+
+### GoBGP
+```bash
+$ unzip ksator_gobgp.zip
+$ mv ksator_gobgp.tar.gz ksator_gobgp.tar
+$ docker load -i ksator_gobgp.tar
+$ docker tag ksator_gobgp:latest ksator/gobgp:1.0
+```
+
+### FRR
+```bash
+$ unzip github_frr10.zip
+$ mv github_frr10.tar.gz github_frr10.tar
+$ docker load -i github_frr10.tar
+$ docker tag github_frr10 github/frr10
+```
+
+### ExaBGP
+```bash
+$ unzip mikenowak_exabgp.zip
+$ mv mikenowak.tar.gz mikenowak_exabgp.tar
+$ docker load -i mikenowak_exabgp.tar
+$ docker tag mikenowak_exabgp:latest mikenowak/exabgp:latest
+```
+
+### Batfish
+Pull the Batfish image as instructed in this [link](https://batfish.readthedocs.io/en/latest/getting_started.html).
--- tester/bgp/rmap_pl/frr/run.sh
+++ tester/bgp/rmap_pl/frr/run.sh
@@ -1,12 +1,12 @@
 PREFIX="$1"
 
-sudo docker compose down
+docker compose down
 
-sudo docker compose up -d
+docker compose up -d
 
 # command to run by forking a new process
 command_to_fork(){
-    sudo docker exec exabgp_1 bash -c "exabgp exabgp/conf.ini"
+    docker exec exabgp_1 bash -c "exabgp exabgp/conf.ini"
 }
 
 command_to_fork &
@@ -19,11 +19,11 @@ sudo docker exec -it frr_2 vtysh -c "clear ip bgp * soft"
 sleep 5
 
 # get the output
-sudo docker exec -it frr_2 vtysh -c "show ip bgp" > full_RIB.txt
-sudo docker exec -it frr_2 vtysh -c "show ip bgp ${PREFIX}" > router2_RIB.txt
+docker exec -it frr_2 vtysh -c "show ip bgp" > full_RIB.txt
+docker exec -it frr_2 vtysh -c "show ip bgp ${PREFIX}" > router2_RIB.txt
 
 # stop the containers and shut down the network
-sudo docker compose down
+docker compose down
 
 # stop all child processes before exiting
 pkill -P $$
